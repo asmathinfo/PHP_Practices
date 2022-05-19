a9350605630b44be48563228889d0a14c7d4486a
Buhary Asmath <asmath.info@gmail.com>
2022-05-19 08:53:47 +0530

    Single / Multi comment lines

---
+++ Comments.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML Comments</title>
+</head>
+<body>
+    <h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+
+<?php
+
+    // Single line Comment line
+    echo "<h1 align ='center'>Hello This Buhary Asmath this is First Programming in PHP</h1>"
+
+    /*
+    Multi 
+    Line 
+    Comment
+    Line
+     */
+?>
+
+    
+</body>
+</html>
