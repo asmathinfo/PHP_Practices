6463cd28dae61c67be1512f1f1d57fd5e2eba68e
Buhary Asmath <asmath.info@gmail.com>
2022-05-19 08:42:10 +0530

    Echo php scripts in HTML

---
+++ Html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP in HTML</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body class="fb">
+    <?php
+    
+    echo "<h1 align='center' >Hello This Buhary Asmath this is First Programming in PHP</h1>";
+    echo "<h1 align='center' >This is about to Echo php scripts in HTML</h1>";
+    echo "Hello This is First Programming in PHP</h1>";
+  
+
+    
+    ?>
+</body>
+</html>
