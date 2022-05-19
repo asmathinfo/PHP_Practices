12eb0d1323a2b458f49bea5ce806fb9d977e44ca
Buhary Asmath <asmath.info@gmail.com>
2022-05-19 09:11:35 +0530

    Basic Variable Delcarations

---
+++ Variables.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Variables </title>
+</head>
+<body>
+    
+<?php
+/*
+Rules for PHP variables:
+
+A variable starts with the $ sign, followed by the name of the variable
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive ($age and $AGE are two different variables)
+
+*/
+
+$pi = 0.5888565;
+$num = 1546;
+$name = "Buhary Asmath ";
+
+
+echo "The Value of PI :".$pi;
+echo "<br>The ID Number is :".$num;
+echo "<br>The Name is :".$name;
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
