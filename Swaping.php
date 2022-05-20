4a360a0e6001f012eb1c0fa4db885f573fa0def4
Buhary Asmath <asmath.info@gmail.com>
2022-05-20 13:06:49 +0530

    Swaping concept

---
+++ Swaping.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swaping in php </title>
+</head>
+<body>
+<h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h3 align ="center">Swaping </h3>
+
+
+<?php
+
+ $a=100;
+ $b=250;
+
+
+ echo "Before Swaping :<br>";
+ echo "A : $a <br>";
+ echo "B : $b <br>";
+
+
+
+$c=$a; //100
+$a=$b; //250
+$b=$c; //100
+
+
+
+ echo "After Swaping :<br>";
+ echo "A : $a <br>";
+ echo "B : $b <br>";
+ 
+
+
+?>
+
+</body>
+</html>
