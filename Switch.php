5ece92c6a1ebc51b336ce11a7023d3f4d1f86d63
Buhary Asmath <asmath.info@gmail.com>
2022-05-20 09:41:04 +0530

    using switch case  for matching values

---
+++ Switch.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Switch </title>
+</head>
+<body>
+<h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h1 align ="center">Hello This Buhary Asmath this is First Programming in PHP</h1>
+    <h3 align ="center">Switch </h3>
+
+
+<?php
+
+  $ab=45;
+switch ($ab)
+{
+    case 1:
+        echo "One";
+        break;        //break using for stop execution 
+
+    case 2:
+        echo "Two";
+        break;
+
+    case 3:
+        echo "Three";
+        break;
+
+    case 4:
+        echo "Four";
+        break;
+
+    case 5:
+        echo "Five";
+        break;
+
+    default:
+        echo "Please Enter Values Between 1-5";
+        break;    
+
+}
+
+
+?>
+
+</body>
+</html>
